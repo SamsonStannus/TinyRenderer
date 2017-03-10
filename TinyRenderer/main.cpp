7da3a7fafb0ab193e8404e8644b130cedffc8ef3
SamsonStannus <samsonstannus@gmail.com>
2017-03-10 17:59:48 -0500

    Initial Commit

---
+++ TinyRenderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TinyRenderer
+//
+//  Created by samson stannus on 2017-03-10.
+//  Copyright (c) 2017 samson stannus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
