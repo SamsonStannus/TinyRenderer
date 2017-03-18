62f40219de636ffcc3955d45e76fb2d0528cea17
SamsonStannus <samsonstannus@gmail.com>
2017-03-18 19:47:49 -0400

    added flat shading - no backface culling

--- TinyRenderer/geometry.h
+++ TinyRenderer/geometry.h
@@ -39,11 +39,11 @@ template <class t> struct Vec3
     };
     Vec3() : x(0), y(0), z(0) {}
     Vec3(t _x, t _y, t _z) : x(_x), y(_y), z(_z) {}
-    inline Vec3<t> operator ^(const Vec2<t> &v) const { return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); }
-    inline Vec3<t> operator +(const Vec2<t> &v) const { return Vec3<t>(x+v.x, y+v.y, z+v.z); }
-    inline Vec3<t> operator -(const Vec2<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
+    inline Vec3<t> operator ^(const Vec3<t> &v) const { return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); }
+    inline Vec3<t> operator +(const Vec3<t> &v) const { return Vec3<t>(x+v.x, y+v.y, z+v.z); }
+    inline Vec3<t> operator -(const Vec3<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
     inline Vec3<t> operator *(float f)          const { return Vec3<t>(x*f, y*f, z*f); }
-    inline t       operator *(const Vec2<t> &v) const { return Vec3<t>(x*v.x + y*v.y + z*v.z); }
+    inline t       operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
     float norm() const { return std::sqrt(x*x + y*y + z*z); }
     Vec3<t> normalize(t l=1) { *this = (*this)*(l/norm()); return *this; }
     template <class > friend std::ostream& operator <<(std::ostream& s, Vec3<t>& v);
--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -6,6 +6,7 @@
 //  Copyright (c) 2017 samson stannus. All rights reserved.
 //
 
+#include <vector>
 #include <iostream>
 #include "tgaimage.h"
 #include "model.h"
@@ -22,27 +23,37 @@ const int height = 800;
 void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color);
 void line(int x0, int y0, int x1, int y1,TGAImage &image, TGAColor color);
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
-void renderModel(Model *model, TGAImage &image, TGAColor color);
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color);
+void renderWireFrame(Model *model, TGAImage &image, TGAColor color);
+void render(Model *model, TGAImage &image, TGAColor color);
+Vec3f cross(Vec3f v1, Vec3f v2);
+Vec3f barycentric(Vec2i *pts, Vec2i P);
 
 int main(int argc, char** argv) {
     
-//    if (2 == argc)
-//    {
-//        model = new Model(argv[1]);
-//    }
-//    else
-//    {
-//        model = new Model("../../../obj/diablo3_pose/diablo3_pose.obj");
-//    }
+    if (2 == argc)
+    {
+        model = new Model(argv[1]);
+    }
+    else
+    {
+        model = new Model("../../../obj/african_head/african_head.obj");
+    }
     TGAImage image(width, height, TGAImage::RGB);
     
-//    renderModel(model, image, white);
-    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+//    renderWireFrame(model, image, white);
+    render(model, image, blue);
+    
+    
+//    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+//    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+//    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+//    triangle(t0[0], t0[1], t0[2], image, red);
+//    triangle(t1[0], t1[1], t1[2], image, white);
+//    triangle(t2[0], t2[1], t2[2], image, green);
+    
+//    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)};
+//    triangle(pts, image, green);
     
     image.flip_vertically();
     std::cout << "Writing File\n";
@@ -127,8 +138,36 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
     }
 }
 
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color)
+{
+    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxmax(0, 0);
+    Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            bboxmin.raw[j] = std::max(0,            std::min(bboxmin.raw[j], pts[i].raw[j]));
+            bboxmax.raw[j] = std::min(clamp.raw[j], std::max(bboxmax.raw[j], pts[i].raw[j]));
+        }
+    }
+    Vec2i P;
+    for (P.x = bboxmin.x; P.x < bboxmax.x; P.x++)
+    {
+        for (P.y = bboxmin.y; P.y < bboxmax.y; P.y++)
+        {
+            Vec3f bcScreen = barycentric(pts, P);
+            if (bcScreen.x < 0 || bcScreen.y < 0 || bcScreen.z < 0) continue;
+            std::cout << P << std::endl;
+            image.set(P.x, P.y, color);
+        }
+    }
+    
+    
+}
+
 // Renders a Model using lines
-void renderModel(Model *model, TGAImage &image, TGAColor color)
+void renderWireFrame(Model *model, TGAImage &image, TGAColor color)
 {
     for (int i = 0; i < model->nfaces(); i++)
     {
@@ -144,5 +183,41 @@ void renderModel(Model *model, TGAImage &image, TGAColor color)
             line(x0, y0, x1, y1, image, color);
         }
     }
+}
+
+void render(Model *model, TGAImage &image, TGAColor color)
+{
+    Vec3f lightDir(0,0,-1);
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screenCoords[3];
+        Vec3f worldCoords[3];
+        for (int j = 0; j < 3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screenCoords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            worldCoords[j] = v;
+        }
+        Vec3f n = (worldCoords[2] - worldCoords[0]) ^ (worldCoords[1] - worldCoords[0]);
+        n.normalize();
+        float intensity = n * lightDir;
+        if (intensity > 0 )
+        {
+            triangle(screenCoords[0], screenCoords[1], screenCoords[2], image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+    }
+}
+
+Vec3f cross(Vec3f v1, Vec3f v2)
+{
+    return Vec3f(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+}
+
+Vec3f barycentric(Vec2i *pts, Vec2i P)
+{
+    Vec3f x = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x);
+    Vec3f y = Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
+    Vec3f u = cross(x, y);
+    if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1); // triangle is degenerate return a negative point.
+    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
 }
 
